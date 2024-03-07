09567824ade41d03daff5e5780b98afc271233b8
mustika chairani <mustikachrn5@gmail.com>
2024-03-07 14:51:31 +0700

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -18,11 +18,14 @@ class _ProfileState extends State<Profile> {
     setState(() {
       _selectedIndex = index;
       if (index == 0) {
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) => homepage()));
-      }else if(index == 1){
-        Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Cart()));
-      }else if(index == 2){
-        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Wishlist()));
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => homepage()));
+      } else if (index == 1) {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => Cart()));
+      } else if (index == 2) {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => Wishlist()));
       }
     });
   }
@@ -33,40 +36,42 @@ class _ProfileState extends State<Profile> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey[200],
-       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed, // Set type to fixed
-        backgroundColor: const Color.fromARGB(
-            255, 193, 24, 24), // Set the background color here
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Wishlist',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_rounded), // Add your new icon here
-            label: 'Profile', // Add the label for the new icon
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color.fromARGB(255, 255, 255, 255),
-        unselectedItemColor: Color.fromARGB(207, 0, 0, 0),
-        onTap: _onItemTapped,
-      ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, // Set type to fixed
+          backgroundColor: const Color.fromARGB(
+              255, 193, 24, 24), // Set the background color here
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Wishlist',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_rounded), // Add your new icon here
+              label: 'Profile', // Add the label for the new icon
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: const Color.fromARGB(255, 255, 255, 255),
+          unselectedItemColor: Color.fromARGB(207, 0, 0, 0),
+          onTap: _onItemTapped,
+        ),
         body: Container(
           padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text('Profile'),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Container(
                 color: Color(0xFFB50B0B),
                 padding: EdgeInsets.all(20),
@@ -120,55 +125,112 @@ class _ProfileState extends State<Profile> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: 15,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Account Information'),
-                        Icon(Icons.settings_outlined),
-                      ],
+                    SizedBox(
+                      height: 15,
                     ),
-                    SizedBox(height: 20,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('My Store'),
-                        Icon(Icons.storefront),
-                      ],
+                    InkWell(
+                      onTap: () {
+                        // Navigasi atau tindakan yang diinginkan saat bagian ini dipencet
+                        print('Account Information pressed');
+                        // Tambahkan navigasi atau tindakan sesuai kebutuhan Anda
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Account Information'),
+                          Icon(Icons.settings_outlined),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 20,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Daftar Transaksi'),
-                        Icon(Icons.compare_arrows_rounded),
-                      ],
+                    SizedBox(
+                      height: 10,
                     ),
-                    SizedBox(height: 20,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Pembayaran'),
-                        Icon(Icons.payment_rounded),
-                      ],
+                    SizedBox(
+                      height: 10,
                     ),
-                    SizedBox(height: 10,),
+                    InkWell(
+                      onTap: () {
+                        // Navigasi atau tindakan yang diinginkan saat bagian ini dipencet
+                        print('My Store pressed');
+                        // Tambahkan navigasi atau tindakan sesuai kebutuhan Anda
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('My Store'),
+                          Icon(Icons.storefront),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // Navigasi atau tindakan yang diinginkan saat bagian ini dipencet
+                        print('Daftar Transaksi pressed');
+                        // Tambahkan navigasi atau tindakan sesuai kebutuhan Anda
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Daftar Transaksi'),
+                          Icon(Icons.compare_arrows_rounded),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // Navigasi atau tindakan yang diinginkan saat bagian ini dipencet
+                        print('Pembayaran pressed');
+                        // Tambahkan navigasi atau tindakan sesuai kebutuhan Anda
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Pembayaran'),
+                          Icon(Icons.payment_rounded),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
                   ],
                 ),
               ),
               SizedBox(
                 height: 10,
               ),
-              Container(
-                
-                height: 60,
-                color: Colors.white,
-                padding: EdgeInsets.all(15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [Text('Logout'), Icon(Icons.logout)],
+              InkWell(
+                onTap: () {
+                  // Tindakan logout, misalnya membersihkan data sesi atau menghapus token
+                  print('Logout pressed');
+
+                  // Navigasi kembali ke halaman login
+                },
+                child: Container(
+                  height: 60,
+                  color: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Logout'),
+                      Icon(Icons.logout),
+                    ],
+                  ),
                 ),
-              ),
+              )
             ],
           ),
         ),
