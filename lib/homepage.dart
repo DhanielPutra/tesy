0c9aa8f476523467370e169c9880d53334a8e05d
mustika chairani <mustikachrn5@gmail.com>
2024-04-18 15:08:07 +0700

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,7 +4,8 @@ import 'package:marketplace/cart.dart';
 import 'package:marketplace/detail.dart';
 import 'package:marketplace/profile.dart';
 import 'package:marketplace/search.dart';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:marketplace/product.dart';
 import 'package:marketplace/wishlist.dart';
 
@@ -19,6 +20,22 @@ class _homepageState extends State<homepage> {
   Color _cardColor = Colors.white;
   String _selectedText = 'All'; // Initially set to 'All'
   List<List<String>> filteredItems = List.from(items);
+  List<dynamic> products = [];
+
+  Future<List<dynamic>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://barbeqshop.online/api/produk'));
+
+    if (response.statusCode == 200) {
+      // Jika berhasil, kembalikan data dari API
+      Map<String, dynamic> responseData = json.decode(response.body);
+      return responseData['data']; // Ambil data dari kunci 'data'
+    } else {
+      // API mengembalikan kode status yang tidak diharapkan
+      print('API mengembalikan kode status: ${response.statusCode}');
+      return []; // Kembalikan list kosong
+    }
+  }
 
   int _selectedIndex = 0;
 
@@ -169,68 +186,89 @@ class _homepageState extends State<homepage> {
             const SizedBox(
               height: 8,
             ),
-            Wrap(
-              spacing: 20, // Gap between cards
-              runSpacing: 8, // Gap between rows
-              children: filteredItems.map((item) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => Detail(item: item)));
-                  },
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width / 2 -
-                        10, // Make each card take half of the screen width
-                    child: Card(
-                      color: _cardColor,
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              item[0], // Accessing image path from items
-                              height: 150,
-                              width: double.infinity,
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ), // Add spacing between image and text
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                item[1], // Accessing text from items
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
+            FutureBuilder<List<dynamic>>(
+              future: fetchData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // Menampilkan indikator loading jika data sedang dimuat
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  // Menampilkan pesan kesalahan jika terjadi kesalahan dalam memuat data
+                  return Center(
+                      child: Text('Terjadi kesalahan: ${snapshot.error}'));
+                } else {
+                  // Menampilkan data produk dari API menggunakan ListView
+                  return Center(
+                    child: Wrap(
+                      spacing: 20, // Jarak antar kartu
+                      runSpacing: 8, // Jarak antar baris
+                      children: snapshot.data!.map<Widget>((product) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => Detail(
+                                item: product,
+                              ),
+                            ));
+                          },
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 2 -
+                                30, // Membuat setiap kartu mengambil separuh lebar layar
+                            child: Card(
+                              color: _cardColor,
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      product['gambar'],
+                                      height: 150,
+                                      width: double.infinity,
+                                    ),
+                                    const SizedBox(
+                                      height: 8,
+                                    ), // Menambahkan jarak antara gambar dan teks
+                                    Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        product['nama_produk'],
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines:
+                                            1, // Batasi teks menjadi 1 baris
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        'Rp ${product['harga']}',
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w400,
+                                          color: Color.fromARGB(255, 2, 2, 2),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1, // Limit the text to 1 line
                               ),
                             ),
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                item[2], // Accessing text from items
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color.fromARGB(255, 2, 2, 2),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                          ),
+                        );
+                      }).toList(),
                     ),
-                  ),
-                );
-              }).toList(),
+                  );
+                }
+              },
             ),
           ],
         ),
