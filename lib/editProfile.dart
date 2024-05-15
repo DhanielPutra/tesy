9d19669df7de2d241466669d82843b39171168b9
DhanielPutra <dhany6654@gmail.com>
2024-05-15 09:50:14 +0700

    edit profile done

--- lib/editProfile.dart
+++ lib/editProfile.dart
@@ -1,16 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 import 'package:marketplace/models/api_response.dart';
 import 'package:marketplace/user_services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class EditProfile extends StatefulWidget {
-  const EditProfile({
-    Key? key,
-  }) : super(key: key);
+  const EditProfile({Key? key}) : super(key: key);
 
   @override
   State<EditProfile> createState() => _EditProfileState();
@@ -20,12 +18,13 @@ class _EditProfileState extends State<EditProfile> {
   TextEditingController namaController = TextEditingController();
   TextEditingController teleponController = TextEditingController();
   TextEditingController emailController = TextEditingController();
-  File? _image; // Variabel untuk menyimpan gambar yang dipilih
+  File? _image;
+  String? _imageUrl;
 
   @override
   void initState() {
     super.initState();
-    // Menggunakan gambar yang disediakan awal sebagai gambar default
+    fetchUserProfile();
   }
 
   Future<void> _getImageFromGallery() async {
@@ -35,74 +34,129 @@ class _EditProfileState extends State<EditProfile> {
     setState(() {
       if (pickedImage != null) {
         _image = File(pickedImage.path);
-        print('Nama gambar terpilih: ${pickedImage.path.split('/').last}');
+        print('Selected image name: ${pickedImage.path.split('/').last}');
       } else {
         print('No image selected.');
       }
     });
   }
 
-  String apiUrl = "https://barbeqshop.online/api/user";
+  Future<void> fetchUserProfile() async {
+    String token = await getToken();
+    try {
+      final response = await http.get(
+        Uri.parse('https://barbeqshop.online/api/profile'),
+        headers: <String, String>{
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
 
-  Future<ApiResponse> updateUser(
-      String name, String email, String phone, String? imageBase64) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? token = prefs.getString(
-        'token'); // Asumsikan kamu menggunakan token untuk autentikasi
+      print('Response status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
 
-    final response = await http.put(
-      Uri.parse(apiUrl),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-      body: jsonEncode({
-        'name': name,
-        'email': email,
-        'phone': phone,
-        'image': imageBase64,
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      return ApiResponse.fromJson(jsonDecode(response.body));
-    } else {
-      return ApiResponse(error: 'Failed to update profile');
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        if (data['success']) {
+          setState(() {
+            namaController.text = data['data']['user']['name'];
+            teleponController.text = data['data']['user']['no_tlp'];
+            emailController.text = data['data']['user']['email'];
+            _imageUrl = data['data']['user']['gambar'];
+          });
+        } else {
+          throw Exception(data['message']);
+        }
+      } else {
+        throw Exception('Failed to load user profile: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching user profile: $error');
     }
   }
 
+  Future<ApiResponse> updateUser(
+    String name, String email, String phone, File? image) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String apiUrl = "https://barbeqshop.online/api/user";
+  String? token = prefs.getString('token');
+
+  try {
+    var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
+    request.headers['Authorization'] = 'Bearer $token';
+
+    request.fields['name'] = name;
+    request.fields['email'] = email;
+    request.fields['no_tlp'] = phone;
+
+    if (image != null) {
+      var stream = http.ByteStream(image.openRead());
+      var length = await image.length();
+      var multipartFile = http.MultipartFile('gambar', stream, length,
+          filename: image.path.split('/').last);
+      request.files.add(multipartFile);
+    }
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      var responseBody = await response.stream.bytesToString();
+      return ApiResponse.fromJson(jsonDecode(responseBody));
+    } else {
+      var responseBody = await response.stream.bytesToString();
+      print('Failed to update profile: $responseBody');
+      return ApiResponse(error: 'Failed to update profile');
+    }
+  } catch (e) {
+    print('Exception occurred while updating profile: $e');
+    return ApiResponse(error: 'An error occurred while updating profile');
+  }
+}
+
+
   Future<void> _updateProfile() async {
-    try {
-      ApiResponse response = await updateUser(
+  try {
+    print('Updating profile with the following data:');
+    print('Name: ${namaController.text}');
+    print('Email: ${emailController.text}');
+    print('Phone: ${teleponController.text}');
+    print('Image: ${_image != null ? 'Image selected' : 'No image selected'}');
+
+    ApiResponse response = await updateUser(
+      namaController.text,
+      emailController.text,
+      teleponController.text,
+      _image,
+    );
+
+    if (response.data != null) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Profile updated successfully'),
+      ));
+      Navigator.pop(context, [
         namaController.text,
         teleponController.text,
         emailController.text,
-        _image != null ? getStringImage(_image!) : null,
-      );
-      if (response.data != null) {
-        // Jika berhasil, tampilkan pesan sukses atau lakukan tindakan lain yang sesuai
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('Profile updated successfully'),
-        ));
-        Navigator.pop(context, [
-          namaController.text,
-          teleponController.text,
-          emailController.text,
-          _image, // Mengirim gambar kembali ke layar Profile
-        ]);
-      } else {
-        // Jika gagal, tampilkan pesan kesalahan
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(response.error ?? 'Failed to update profile'),
-        ));
-      }
-    } catch (e) {
-      // Tangani kesalahan jika terjadi
-      print('Error updating profile: $e');
+        _image,
+      ]);
+    } else {
+      print('Update profile failed with error: ${response.error}');
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('An error occurred while updating profile'),
+        content: Text(response.error ?? 'Failed to update profile'),
       ));
     }
+  } catch (e) {
+    print('Error updating profile: $e');
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('An error occurred while updating profile'),
+    ));
+  }
+}
+
+
+  String getStringImage(File image) {
+    final bytes = image.readAsBytesSync();
+    return base64Encode(bytes);
   }
 
   @override
@@ -117,19 +171,18 @@ class _EditProfileState extends State<EditProfile> {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: const Icon(Icons.arrow_back),
+          icon: Icon(Icons.arrow_back),
         ),
-        title: const Center(
-            child: Text(
-          'Edit Profil',
-          style: TextStyle(color: Colors.white),
-        )),
+        title: Center(
+          child: Text(
+            'Edit Profil',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
         actions: [
           TextButton(
-            onPressed: () {
-              _updateProfile();
-            },
-            child: const Text(
+            onPressed: _updateProfile,
+            child: Text(
               'SAVE',
               style: TextStyle(color: Colors.white),
             ),
@@ -148,10 +201,9 @@ class _EditProfileState extends State<EditProfile> {
                     children: [
                       Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        //gambar
                         children: [
                           Container(
-                            padding: const EdgeInsets.all(50),
+                            padding: EdgeInsets.all(50),
                             decoration: BoxDecoration(
                               color: Color.fromARGB(255, 201, 11, 11),
                             ),
@@ -163,12 +215,11 @@ class _EditProfileState extends State<EditProfile> {
                                   radius: 80.0,
                                   backgroundColor: Colors.grey,
                                   backgroundImage: _image != null
-                                      ? (_image!.path.startsWith('http')
-                                          ? NetworkImage(_image!.path)
-                                              as ImageProvider
-                                          : FileImage(_image!))
-                                      : null,
-                                  child: _image == null
+                                      ? FileImage(_image!)
+                                      : _imageUrl != null
+                                          ? NetworkImage(_imageUrl!) as ImageProvider
+                                          : null,
+                                  child: _image == null && _imageUrl == null
                                       ? Icon(Icons.person, size: 80)
                                       : null,
                                 ),
@@ -181,9 +232,8 @@ class _EditProfileState extends State<EditProfile> {
                   ),
                 ),
                 Container(
-                  padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
+                  padding: EdgeInsets.fromLTRB(30, 20, 30, 10),
                   child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text('Fullname'),
@@ -191,17 +241,13 @@ class _EditProfileState extends State<EditProfile> {
                         controller: namaController,
                         decoration: InputDecoration(),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
+                      SizedBox(height: 10),
                       Text('Phone number'),
                       TextField(
                         controller: teleponController,
                         decoration: InputDecoration(),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
+                      SizedBox(height: 10),
                       Text('Email'),
                       TextField(
                         controller: emailController,
