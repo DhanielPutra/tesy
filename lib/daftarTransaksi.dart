da739c807a5b239fb145b0ae725ea3f1a1a678d6
DhanielPutra <dhany6654@gmail.com>
2024-05-17 16:20:19 +0700

    apply api and make sorting for daftar transaksi done, Fixing UI next

--- lib/daftarTransaksi.dart
+++ lib/daftarTransaksi.dart
@@ -2,13 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:marketplace/user_services.dart';
-import 'package:indexed/indexed.dart';
-import 'package:marketplace/product.dart';
-import 'package:marketplace/profile.dart';
-import 'package:marketplace/rincianPesanan.dart';
 
 class DaftarTransaksi extends StatefulWidget {
-  const DaftarTransaksi({super.key});
+  const DaftarTransaksi({Key? key}) : super(key: key);
 
   @override
   State<DaftarTransaksi> createState() => _DaftarTransaksiState();
@@ -16,10 +12,7 @@ class DaftarTransaksi extends StatefulWidget {
 
 class _DaftarTransaksiState extends State<DaftarTransaksi> {
   List<dynamic>? items;
-  List<dynamic>? inProcessItems;
-  List<dynamic>? completedItems;
-  List<dynamic>? canceledItems;
-  int _selectedIndex = 0; // Variabel untuk menyimpan indeks yang dipilih
+  int _selectedStatusIndex = 0; // Index for selected status type
 
   @override
   void initState() {
@@ -35,21 +28,21 @@ class _DaftarTransaksiState extends State<DaftarTransaksi> {
       final response = await http.get(
         Uri.parse(url),
         headers: {
-          'Authorization': 'Bearer $token'
-        }, // Pass the token in the headers
+          'Authorization': 'Bearer $token' // Include the token in the request headers
+        },
       );
-      ;
 
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body)['data'];
+        // Sort the data based on status_id
+        data.sort((a, b) {
+          int statusIdA = int.tryParse(a['status_id'].toString()) ?? 0;
+          int statusIdB = int.tryParse(b['status_id'].toString()) ?? 0;
+          return statusIdA.compareTo(statusIdB);
+        });
         setState(() {
           items = data;
-          inProcessItems =
-              items!.where((item) => item['status_id'] == '1').toList();
-          completedItems =
-              items!.where((item) => item['status_id'] == '2').toList();
-          canceledItems =
-              items!.where((item) => item['status_id'] == '3').toList();
+          print(data);
         });
       } else {
         throw Exception('Failed to load items');
@@ -61,121 +54,146 @@ class _DaftarTransaksiState extends State<DaftarTransaksi> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Daftar Transaksi'),
-          leading: IconButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => Profile()));
-              },
-              icon: Icon(Icons.arrow_back)), // Tombol kembali di AppBar
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Bagian atas: tombol navigasi
-            Indexed(
-              index: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children:
-                      ['Dalam Proses', 'Selesai'].asMap().entries.map((entry) {
-                    final int index = entry.key;
-                    final String label = entry.value;
-                    return InkWell(
-                      onTap: () {
-                        setState(() {
-                          _selectedIndex =
-                              index; // Mengatur indeks yang dipilih saat tombol diklik
-                        });
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              label,
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: _selectedIndex == index
-                                    ? Colors.blue
-                                    : Colors.black,
-                              ),
-                            ),
-                            Container(
-                              width: 100, // Lebar garis bawah
-                              height: 2,
-                              color: _selectedIndex == index
-                                  ? Colors.blue
-                                  : Colors.transparent,
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
-            // Bagian tengah: konten sesuai dengan indeks yang dipilih
-            Expanded(
-              child: ListView.builder(
-                itemCount: _getItemsForSelectedIndex().length,
-                itemBuilder: (BuildContext context, int index) {
-                  final product = _getItemsForSelectedIndex()[index];
-                  return GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      height: 200,
-                      width: double.infinity,
-                      child: Card(
-                        margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0),
-                        ),
-                        child: Column(
-                          children: [
-                            product['gambar'] != null
-                                ? Image.network(
-                                    product['gambar'],
-                                    width: 150,
-                                    height: double.infinity,
-                                  )
-                                : Container(),
-                          ],
-                        ),
-                      ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Daftar Transaksi'),
+      ),
+      body: Column(
+        children: [
+          // Segment control for selecting status type
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedStatusIndex = 0;
+                      });
+                    },
+                    child: const Text('Diproses'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _selectedStatusIndex == 0 ? Colors.blue : null,
                     ),
-                  );
-                  // return ListTile(
-                  //   title: Text(product['alamat'] ?? ''),
-                  //   subtitle: Text(product['cara_bayar'] ?? ''),
-                  // );
-                },
-              ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedStatusIndex = 1;
+                      });
+                    },
+                    child: const Text('Dikirim'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _selectedStatusIndex == 1 ? Colors.blue : null,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedStatusIndex = 2;
+                      });
+                    },
+                    child: const Text('Selesai'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: _selectedStatusIndex == 2 ? Colors.blue : null,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items?.length ?? 0,
+              itemBuilder: (BuildContext context, int index) {
+                final item = items![index];
+                final product = item['produk'];
+
+                // Check if the item matches the selected status type
+                if (_selectedStatusIndex == 0) {
+                  // Show items with status ID '1' or null
+                  if (item['status_id'] != '1' && item['status_id'] != null) {
+                    return Container(); // Return empty container if not 'Diproses'
+                  }
+                } else if (_selectedStatusIndex == 1 && item['status_id'] != '2') {
+                  return Container(); // Return empty container if not 'Dikirim'
+                } else if (_selectedStatusIndex == 2 && item['status_id'] != '3') {
+                  return Container(); // Return empty container if not 'Selesai'
+                }
+
+                return GestureDetector(
+                  onTap: () {
+                    // Navigate to transaction detail page here
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 120,
+                          height: 120,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(product['gambar']),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                product['nama_produk'],
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Price: Rp. ${product['harga']}',
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Detail: ${product['detail']}',
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
-  }
-
-  List<dynamic> _getItemsForSelectedIndex() {
-    switch (_selectedIndex) {
-      case 0:
-        return inProcessItems ?? [];
-      case 1:
-        return completedItems ?? [];
-      case 2:
-        return canceledItems ?? [];
-      default:
-        return [];
-    }
   }
 }
